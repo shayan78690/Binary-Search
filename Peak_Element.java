62d774603bc51598804d473553c036a896632dd5
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-01-14 23:49:06 +0530

    Update Peak_Element.java

--- Peak_Element.java
+++ Peak_Element.java
@@ -1,12 +1,27 @@
-import java.util.ArrayList;
-public class Solution {
-    public static int findPeakElement(ArrayList<Integer> arr) {
-        int n = arr.size();
-        for(int i = 0; i < n; i++) {
-            if((i == 0 || arr.get(i-1) < arr.get(i)) && (i == n-1 || arr.get(i) > arr.get(i+1))) {
-                return i;
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if(n == 1) {
+            return 0;
+        }
+        if(nums[0] > nums[1]) {
+            return 0;
+        }
+        if(nums[n-1] > nums[n-2]) {
+            return n-1;
+        }
+        int low = 1;
+        int high = n-2;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
+                return mid;
+            } else if(nums[mid] > nums[mid-1]) {
+                low = mid + 1;
+            } else{
+                high = mid - 1;
             }
         }
         return -1;
     }
-};
+}
