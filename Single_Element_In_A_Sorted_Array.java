925ea47c715237d2be5e8435e6cecaeda33a3fb0
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-01-14 22:02:28 +0530

    Update Single_Element_In_A_Sorted_Array.java

--- Single_Element_In_A_Sorted_Array.java
+++ Single_Element_In_A_Sorted_Array.java
@@ -1,9 +1,27 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int XOR = 0;
-        for(int i = 0; i < nums.length; i++) {
-            XOR = XOR ^ nums[i];
+        if(nums.length == 1) {
+            return nums[0];
         }
-        return XOR;
+        if(nums[0] != nums[1]) {
+            return nums[0];
+        }
+        if(nums[nums.length-1] != nums[nums.length-2]) {
+            return nums[nums.length-1];
+        }
+        int low = 1;
+        int high = nums.length - 2;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]) {
+                return nums[mid];
+            }
+            if((mid % 2 == 1 && nums[mid] == nums[mid-1]) || (mid % 2 == 0 && nums[mid] == nums[mid+1])) {
+                low = mid + 1;
+            } else{
+                high = mid-1;
+            }
+        }
+        return -1;
     }
 }
