6d4e6188f521fca937097af34be9a8b5ca954b25
Mo Shayan Ul Haque <shayaanansari80@gmail.com>
2025-01-22 00:24:06 +0530

    Update Kth_Missing_Positive_Number.java

--- Kth_Missing_Positive_Number.java
+++ Kth_Missing_Positive_Number.java
@@ -1,16 +1,16 @@
-class Solution {
-    public int findKthPositive(int[] arr, int k) {
-        int n = arr.length;
-        int low = 0, high = n - 1;
-        while (low <= high) {
+public class Solution {
+    public static int missingK(int[] vec, int n, int k) {
+        int low = 0;
+        int high = n-1;
+        while(low <= high) {
             int mid = (low + high) / 2;
-            int missing = arr[mid] - (mid + 1);
-            if (missing < k) {
+            int missing = vec[mid] - (mid + 1);
+            if(missing < k) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
-        return k + high + 1;
+        return low + k;
     }
 }
